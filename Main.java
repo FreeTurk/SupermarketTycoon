5bb26974a3e563aca4cb5851f3ccebbd81d60fdc
Emir Oven <teknolojigamers@gmail.com>
2024-10-21 20:02:06 +0200

    Set a simple window layout out

--- Main.java
+++ Main.java
@@ -1,9 +1,43 @@
-class Main {
+import javax.swing.*;
+import java.awt.*;
+
+class Main extends JFrame {
+
+  public Main() {
+    // Creates the layout for the window
+    GridBagLayout layout = new GridBagLayout();
+    layout.columnWeights = new double[] { 0.0, 1.0 };
+    layout.rowWeights = new double[] { 1.0 };
+    layout.columnWidths = new int[] { 400, 0 };
+
+    // Creates the panel that will hold the current page
+    JPanel currentPagePanel = new JPanel();
+    CardLayout cardLayout = new CardLayout();
+    currentPagePanel.setLayout(cardLayout);
+
+    // Creates the sidebar
+    Sidebar sidebar = new Sidebar();
+    Stocks stocks = new Stocks();
+
+    currentPagePanel.add("stocks", stocks);
+
+    // Sets the layout of the main window
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(layout);
+    setSize(1920, 1200);
+
+    // Adds the sidebar to the main window
+    add(sidebar.initialize_page());
+    add(currentPagePanel);
+
+    // Make the window visible
+    setLocationRelativeTo(null);
+    setVisible(true);
+
+  }
 
   public static void main(String[] args) {
-    Stocks stocksPage = new Stocks();
-
-    stocksPage.initialize_page();
+    new Main();
   }
 
 }
---
+++ Sidebar.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Sidebar
+ */
+public class Sidebar {
+
+  public JPanel initialize_page() {
+
+    JButton stocksPageButton = new JButton("Stocks");
+
+    JPanel sidebar_panel = new JPanel();
+    sidebar_panel.setLayout(new BoxLayout(sidebar_panel, BoxLayout.Y_AXIS));
+
+    sidebar_panel.add(stocksPageButton);
+
+    return sidebar_panel;
+  }
+
+}
