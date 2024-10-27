f8795d093fff5a6bfd3a92e3f4ac2bf948e53d2f
Emir Oven <teknolojigamers@gmail.com>
2024-10-27 02:36:12 +0200

    Auto stash before merge of "frontend" and "origin/frontend"

--- app/src/main/java/org/supermarkettycoon/Main.java
+++ app/src/main/java/org/supermarkettycoon/Main.java
@@ -5,65 +5,65 @@ import java.awt.*;
 
 class Main extends JFrame {
 
-  public Main() {
-    super("Supermarket Tycoon");
+    public Main() {
+        super("Supermarket Tycoon");
 
-    // Creates the layout for the window
-    GridBagLayout layout = new GridBagLayout();
-    layout.columnWeights = new double[] { 0.3f, 0.5f, 0.2f };
-    layout.rowWeights = new double[] { 0.0, 1.0 };
-    layout.rowHeights = new int[] { 50, 0 };
+        // Creates the layout for the window
+        GridBagLayout layout = new GridBagLayout();
+        layout.columnWeights = new double[]{0.3f, 0.5f, 0.2f};
+        layout.rowWeights = new double[]{0.0, 1.0};
+        layout.rowHeights = new int[]{50, 0};
 
-    Globals globals = new Globals();
+        Globals globals = new Globals();
 
-    TopBar topBar = new TopBar(globals);
-    Stocks stocks = new Stocks(globals);
-    Upgrades upgrades = new Upgrades();
-    SpriteScreen spriteScreen = new SpriteScreen();
-    GridBagConstraints c = new GridBagConstraints();
+        TopBar topBar = new TopBar(globals);
+        Stocks stocks = new Stocks(globals);
+        Upgrades upgrades = new Upgrades();
+        SpriteScreen spriteScreen = new SpriteScreen();
+        GridBagConstraints c = new GridBagConstraints();
 
-    // Sets the layout of the main window
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLayout(layout);
-    setSize(1920, 1200);
+        // Sets the layout of the main window
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(layout);
+        setSize(1920, 1200);
 
-    c.fill = GridBagConstraints.BOTH;
+        c.fill = GridBagConstraints.BOTH;
 
-    c.gridheight = 1;
-    c.gridwidth = 1;
-    c.gridx = 0;
-    c.gridy = 1;
-    add(stocks, c);
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridx = 0;
+        c.gridy = 1;
+        add(stocks, c);
 
-    c.gridheight = 1;
-    c.gridwidth = 2;
-    c.gridx = 0;
-    c.gridy = 0;
-    add(topBar, c);
+        c.gridheight = 1;
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 0;
+        add(topBar, c);
 
-    c.gridheight = 1;
-    c.gridwidth = 1;
-    c.gridheight = 1;
-    c.gridx = 1;
-    c.gridy = 1;
-    add(spriteScreen, c);
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.gridx = 1;
+        c.gridy = 1;
+        add(spriteScreen, c);
 
-    c.gridheight = 2;
-    c.gridwidth = 1;
-    c.gridx = 2;
-    c.gridy = 0;
-    add(upgrades, c);
+        c.gridheight = 2;
+        c.gridwidth = 1;
+        c.gridx = 2;
+        c.gridy = 0;
+        add(upgrades, c);
 
-    // Adds the sidebar to the main window
+        // Adds the sidebar to the main window
 
-    // Make the window visible
-    setLocationRelativeTo(null);
-    setVisible(true);
+        // Make the window visible
+        setLocationRelativeTo(null);
+        setVisible(true);
 
-  }
+    }
 
-  public static void main(String[] args) {
-    new Main();
-  }
+    public static void main(String[] args) {
+        new Main();
+    }
 
 }
--- app/src/main/java/org/supermarkettycoon/SpriteScreen.java
+++ app/src/main/java/org/supermarkettycoon/SpriteScreen.java
@@ -1,15 +1,81 @@
 package org.supermarkettycoon;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.io.IOException;
+import java.util.Objects;
 
-public class SpriteScreen extends JPanel {
-  public SpriteScreen() {
-    GridBagLayout layout = new GridBagLayout();
-    GridBagConstraints c = new GridBagConstraints();
+public class SpriteScreen extends JPanel implements ActionListener {
+    private int centerx = (getWidth() / 2);
+    private int centery = (getWidth() / 2);
 
-    setLayout(layout);
+    private int radius = 200;
 
-    setBackground(Color.BLUE);
-  }
+    private double angle = 0;
+
+    private Timer timer;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
+        Image centralImage;
+
+        try {
+            centralImage = ImageIO.read(Objects.requireNonNull(getClass().getResource("assets/Cart.png")));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        int sizex = 200;
+        int sizey = 200;
+
+        int relcenterx = centerx - sizex / 2;
+        int relcentery = centery - sizey / 2;
+        g.drawImage(Objects.requireNonNull(centralImage), relcenterx, relcentery, sizex, sizey, null);
+
+
+        int x = relcenterx + (int) (radius * Math.cos(angle));
+        int y = relcentery + (int) (radius * Math.sin(angle));
+
+        g.drawImage(Objects.requireNonNull(centralImage), x, y, 200, 200, null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(getHeight());
+
+
+        angle += 0.02;
+        angle = angle % (2 * Math.PI);
+        repaint();
+    }
+
+    public SpriteScreen() {
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+
+        setLayout(layout);
+
+        setOpaque(true);
+        setBackground(Color.WHITE);
+
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                centerx = (getWidth() / 2);
+                centery = (getHeight() / 2);
+            }
+        });
+
+        timer = new Timer(16, this);
+        timer.start();
+    }
 }
